402437525fd1787dee11ae8cdd912169caa2a985
Praneet Nigam <nigampraneet460@gmail.com>
2017-08-19 02:44:26 +0530

    Add files via upload

---
+++ Project2.py
@@ -0,0 +1,129 @@
+import turtle
+def name():
+    window=turtle.Screen()
+    window.bgcolor("black")
+
+    brad=turtle.Turtle()
+    brad.shape("triangle")
+    brad.color("white")
+    brad.speed(6)
+
+    brad.forward(50)
+    brad.left(90)
+    brad.forward(50)
+    brad.left(90)
+    brad.forward(50)
+    brad.left(90)
+    brad.forward(120)
+
+    brad.left(90)
+    brad.forward(70)
+    brad.left(90)
+    brad.forward(120)
+    brad.right(90)
+    brad.forward(50)
+    brad.right(90)
+    brad.forward(50)
+    brad.right(90)
+    brad.forward(50)
+    brad.left(125)
+    brad.forward(85)
+
+    brad.left(55)
+    brad.forward(20)
+    brad.left(75)
+    brad.forward(120)
+    brad.right(150)
+    brad.forward(60)
+    brad.right(100)
+    brad.forward(30)
+    brad.right(180)
+    brad.forward(30)
+    brad.right(80)
+    brad.forward(63)
+
+    brad.left(75)
+    brad.forward(20)
+    brad.left(90)
+    brad.forward(120)
+    brad.right(150)
+    brad.forward(140)
+    brad.left(150)
+    brad.forward(120)
+
+    brad.right(90)
+    brad.forward(70)
+    brad.right(180)
+    brad.forward(50)
+    brad.left(90)
+    brad.forward(60)
+    brad.left(90)
+    brad.forward(25)
+    brad.right(180)
+    brad.forward(25)
+    brad.left(90)
+    brad.forward(60)
+    brad.left(90)
+    brad.forward(70)
+
+    brad.left(90)
+    brad.forward(120)
+    brad.right(90)
+    brad.forward(50)
+    brad.right(180)
+    brad.forward(50)
+    brad.left(90)
+    brad.forward(60)
+    brad.left(90)
+    brad.forward(25)
+    brad.right(180)
+    brad.forward(25)
+    brad.left(90)
+    brad.forward(60)
+    brad.left(90)
+    brad.forward(80)
+
+    brad.left(90)
+    brad.forward(120)
+    brad.right(90)
+    brad.forward(25)
+    brad.right(180)
+    brad.forward(50)
+
+    brad.right(180)
+    brad.forward(25)
+    brad.right(90)
+    brad.forward(120)
+    brad.left(90)
+    brad.forward(40)
+
+    window.exitonclick()
+name()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
+    
+
+    
+
+
+
+
+
